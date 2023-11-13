2c41d5329fda2c43c7601569fc8a3e8d31069d57
Pedro Salas <pedrosalasgalindo@gmail.com@>
2023-11-12 21:55:34 -0600

    webhool

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post,Get } from '@nestjs/common';
 
 @Controller('webhook')
 export class WebhookController {
 
+    //var to save webhooks in memory
+    private webhooks: any[] = [];
+
   @Post()
   handleWebhook(@Body() payload: any): string {
-    // Aquí puedes manejar la lógica relacionada con el webhook
-    console.log('Payload recibido:', payload);
+
+    //save webhook in memory
+    this.webhooks.push(payload);
 
     // Puedes devolver una respuesta, si es necesario
     return 'Webhook recibido con éxito payload: ' + JSON.stringify(payload);
   }
+
+  //method get to get all webhooks saved
+    @Get()
+    async findAll(): Promise<any[]> {
+        return this.webhooks;
+    }
 
 
 }
